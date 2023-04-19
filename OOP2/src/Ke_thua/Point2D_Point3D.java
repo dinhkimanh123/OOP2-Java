ed3534f678323a6d93175714ccf4839a4c72cc18
dinhkimanh123 <118419564+dinhkimanh123@users.noreply.github.com>
2023-04-19 08:11:29 +0700

    Add files via upload

---
+++ OOP2/src/Ke_thua/Point2D_Point3D.java
@@ -0,0 +1,40 @@
+package Ke_thua;
+
+public class Point2D_Point3D {
+
+}
+class poit2D {
+    float x;
+    float y;
+    public poit2D(){
+        this.x = 0.0f;
+        this.y = 0.0f;
+    }
+    public poit2D(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    public void setXY(float x, float y){
+        this.x= x;
+        this.y = y;
+    }
+
+
+
+}
